428fdbd90b55c685ddcdef2deb4ae7376100f0d5
A M <bugzillauser11@gmail.com>
2014-09-14 15:15:34 -0600

    Added validation

--- SimpleRegisterScreen/SRSViewController.m
+++ SimpleRegisterScreen/SRSViewController.m
@@ -32,14 +32,52 @@
     return NO;
 }
 
+-(void)textFieldDidBeginEditing:(UITextField *)textField {
+    textField.backgroundColor = [UIColor clearColor];
+}
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
 - (IBAction)submit:(id)sender {
+    if([self validate]) {
+        NSLog(@"Great!");
+    }
 }
 
 - (IBAction)avatarImageTapped:(id)sender {
+}
+
+#pragma mark Sending Request
+-(BOOL)validate {
+    NSString *emailRegex =
+    @"(?:[a-z0-9!#$%\\&'*+/=?\\^_`{|}~-]+(?:\\.[a-z0-9!#$%\\&'*+/=?\\^_`{|}"
+    @"~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\"
+    @"x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-"
+    @"z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5"
+    @"]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-"
+    @"9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21"
+    @"-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+    return [self validateTextField:self.usernameField forRegexp:@"^.+$"]
+            & [self validateTextField:self.emailField forRegexp:emailRegex]
+            & [self validateTextField:self.passwordField forRegexp:@"^.+$"];
+}
+
+-(BOOL)validateTextField:(UITextField*)field forRegexp:(NSString*)regexp {
+    NSPredicate* test = [NSPredicate predicateWithFormat:@"SELF MATCHES[c] %@", regexp];
+    if([test evaluateWithObject:field.text]) {
+        return YES;
+    }
+    else {
+        field.backgroundColor = [[UIColor redColor] colorWithAlphaComponent:0.2];
+        return NO;
+    }
+}
+
+-(void)sendRequest {
+    
 }
 @end
